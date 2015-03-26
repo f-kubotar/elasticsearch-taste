696f05e3a63bdab961d4a1b26cd7285c02d0593b
f-kubotar <f.kubotar@paperboy.co.jp>
2015-03-26 18:50:34 +0900

    remove GenTermValuesHandler.java lambda

--- src/main/java/org/codelibs/elasticsearch/taste/river/handler/GenTermValuesHandler.java
+++ src/main/java/org/codelibs/elasticsearch/taste/river/handler/GenTermValuesHandler.java
@@ -364,8 +364,12 @@ public class GenTermValuesHandler extends ActionHandler {
 
                 final CountDownLatch genTVGate = new CountDownLatch(numOfThread);
                 for (int i = 0; i < numOfThread; i++) {
-                    ForkJoinPool.commonPool().execute(
-                            () -> processEvent(eventMapQueue, genTVGate));
+                    ForkJoinPool.commonPool().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            processEvent(eventMapQueue, genTVGate);
+                        }
+                    });
                 }
                 gateList.add(genTVGate);
             } finally {
@@ -385,12 +389,24 @@ public class GenTermValuesHandler extends ActionHandler {
             for (int i = 0; i < requestHandlers.length; i++) {
                 handlers[i] = requestHandlers[i];
             }
-            handlers[requestHandlers.length] = (params, listener, requestMap,
-                    paramMap, chain) -> processEvent(eventMapQueue, genTVGate);
-            new RequestHandlerChain(handlers).execute(eventParams, t -> {
-                logger.error("Failed to store: " + eventMap, t);
-                processEvent(eventMapQueue, genTVGate);
-            }, eventMap, new HashMap<>());
+            handlers[requestHandlers.length] = new RequestHandler() {
+                @Override
+                public void execute(Params params,
+                                    RequestHandler.OnErrorListener listener,
+                                    Map<String, Object> requestMap,
+                                    Map<String, Object> paramMap,
+                                    RequestHandlerChain chain) {
+                    processEvent(eventMapQueue, genTVGate);
+                }
+            };
+            final RequestHandler.OnErrorListener errorListener = new RequestHandler.OnErrorListener() {
+                @Override
+                public void onError(Throwable t) {
+                    logger.error("Failed to store: " + eventMap, t);
+                    processEvent(eventMapQueue, genTVGate);
+                }
+            };
+            new RequestHandlerChain(handlers).execute(eventParams, errorListener, eventMap, new HashMap<>());
         }
 
         @Override
